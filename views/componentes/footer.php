f62861a5df338f90cb0fa5f8671aff2c5a73a489
joseveronez <jozeveronez@gmail.com>
2017-12-20 15:28:44 -0200

    Footer
    Ajustes Texto

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -4,8 +4,8 @@
             <div class="col-lg-2 col-md-2 hidden-sm hidden-xs">
                 <img src="<?= RAIZSITE ?>/imagens/logo_footer.png" class="img-responsive MarginT4p" style="">
             </div>
-            <div class="col-lg-4 col-md-4 col-md-offset-5 col-xs-12">
-                <h6 class="branco-fonte Light Uppercase MarginT4p">Av. RepÚBLICA Argentina, 1160 - sala 1010 - Vila Izabel Curitiba - PR, 80620-010 - (41) 2101 4946</h6>
+            <div class="col-lg-4 col-md-4 col-md-offset-5 col-xs-12 text-center-xs">
+                <h6 class="branco-fonte Light Uppercase MarginT4p">Av. RepÚBLICA Argentina, 1160 - sala 1010 - Vila Izabel <br> Curitiba - PR, 80620-010 - (41) 2101 4946</h6>
             </div>
         </div>
     </div>
