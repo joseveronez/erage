dd8d5ad8b64d1589fc8dbbf5d56a86b1a46f23f9
joseveronez <jozeveronez@gmail.com>
2017-12-20 15:21:54 -0200

    Mobile
    Ajustes

--- views/landing.php
+++ views/landing.php
@@ -5,8 +5,8 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-landing" style="background-image: url(<?= RAIZSITE ?>/imagens/landing.jpg)">
     <div class="container Relative" style="height: 100%">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-md-offset-6 col-sm-12 col-xs-12">
-                <img src="<?= RAIZSITE ?>/imagens/logo.png" class="img-responsive MarginT15p" style="height: 120px; margin: 0 auto;">
+            <div class="col-lg-6 col-md-6 col-md-offset-6 col-sm-10 col-xs-10 col-xs-offset-1 bg-erage">
+                <img src="<?= RAIZSITE ?>/imagens/logo.png" class="img-responsive MarginLogo" style="height: 120px; margin: 0 auto;">
                 <h5 class="text-center BikoBold azul-fonte">NOSSO SITE ESTÁ EM DESENVOLVIMENTO</h5>
                 <h6 class="text-center Biko Light azul-fonte Uppercase">Enquanto isso acesse o site da <a href="#" class="azul-fonte BikoBold">CONSTRUTORA EDSON DAMIANI</a> para ver os empreendimentos do grupo.
                 </h6>
