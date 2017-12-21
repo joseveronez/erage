ed61c60b7e94f176e466c5f645e5488aa0f93fb6
joseveronez <jozeveronez@gmail.com>
2017-12-21 11:02:40 -0200

    Site
    Link para a contrutora Damiani
    Link para a apresentação do  Solatium

--- views/landing.php
+++ views/landing.php
@@ -8,13 +8,13 @@
             <div class="col-lg-6 col-md-6 col-md-offset-6 col-sm-10 col-xs-10 col-xs-offset-1 bg-erage">
                 <img src="<?= RAIZSITE ?>/imagens/logo.png" class="img-responsive MarginLogo" style="height: 120px; margin: 0 auto;">
                 <h5 class="text-center BikoBold azul-fonte size17 letter0-5 MarginT4p MarginB2p">NOSSO SITE ESTÁ EM DESENVOLVIMENTO</h5>
-                <h6 class="text-center Biko Light azul-fonte Uppercase line-height1-3">Enquanto isso acesse o site da <a href="#" class="azul-fonte BikoBold">CONSTRUTORA EDSON DAMIANI</a> para ver os empreendimentos do grupo.
+                <h6 class="text-center Biko Light azul-fonte Uppercase line-height1-3">Enquanto isso acesse o site da <a href="http://www.edsondamiani.com.br/" target="_blank" class="azul-fonte BikoBold">CONSTRUTORA EDSON DAMIANI</a> para ver os empreendimentos do grupo.
                 </h6>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-8 col-xs-10 bg-solarium Absolute position-solatium">
                 <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
                     <img src="<?= RAIZSITE ?>/imagens/solatium.png" class="img-responsive MarginT1p" style="margin:0 auto">
-                    <a href="#">
+                    <a href="<?= RAIZSITE ?>/apresentacao/Apresentacao-Solatium.pdf" target="_blank">
                         <img src="<?= RAIZSITE ?>/imagens/download.png" class="img-responsive MarginDownload" style="margin:0 auto; width:30%;">
                     </a>
                     <h6 class="Biko branco-fonte text-center">DOWNLOAD <br> APRESENTAÇÃO</h6>
